bc1752b22cd692cb0d7db18352035614a7d8d8f4
m-inan <muhammet@tilpark.com>
2020-10-11 00:24:38 +0300

    start first apollo example

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,27 @@
+import { ApolloServer, gql } from 'apollo-server';
+
+const typeDefs = gql`
+	type Message {
+		text: String!
+	}
+
+	type Query {
+		message: Message!
+	}
+`;
+
+const resolvers = {
+	Query: {
+		message: () => ({
+			text: 'Hello World!',
+		}),
+	},
+};
+
 function main() {
-	return 0;
+	const server = new ApolloServer({ typeDefs, resolvers });
+
+	server.listen();
 }
 
 main();
