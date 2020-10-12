a31d3f7647d4d65a2db78c93afff225b68e1fac0
m-inan <muhammet@tilpark.com>
2020-10-13 00:56:25 +0300

    start initial express boilerplate

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,54 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import compress from 'compression';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+
 function main() {
-	return 0;
+	/**
+	 * Express instance
+	 * @public
+	 */
+	const app = express();
+	const PORT = 4000;
+
+	// request logging. dev: console | production: file
+	app.use(morgan('combined'));
+
+	// parse body params and attache them to req.body
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
+	// gzip compression
+	app.use(compress());
+
+	// secure apps by setting various HTTP headers
+	app.use(helmet());
+
+	// cookie parser
+	app.use(cookieParser());
+
+	// enable CORS - Cross Origin Resource Sharing
+	app.use(
+		cors({
+			credentials: true,
+		})
+	);
+
+	// static files
+	app.use(express.static('static'));
+
+	app.get('/', (_req: Request, res: Response) => {
+		res.json({
+			success: true,
+			message: 'Rest Api Express',
+		});
+	});
+
+	app.listen(PORT, () => {
+		console.log(`Server Started PORT ${PORT}`);
+	});
 }
 
 main();
